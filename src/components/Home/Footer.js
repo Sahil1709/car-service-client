62a3a42b418a522dcf4bdaf12807d8772c5ee389
Sahil <sahilofficial17@gmail.com>
2021-07-25 13:05:54 +0530

    first commit

---
+++ src/components/Home/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+import { MDBContainer, MDBFooter } from "mdbreact";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-copyright text-center py-3">
+        <h5>&copy; {new Date().getFullYear()} Copyright: Made By Sahil Gupta</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/services/customer/customer_service.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import authHeader from "./authentication/auth_header";
+
+const ORDER_URL = "https://car-service-order.herokuapp.com/order/";
+const CUST_ORDER = "https://car-service-customer.herokuapp.com/customer/order/";
+
+class CustomerService {
+  placeOrder(customerId, customerName, carName, carNumber, custAddress, serviceName, servicePrice) {
+    return axios
+      .post(
+        ORDER_URL + "addOrder",
+        {
+          customerId,
+          customerName,
+          carName,
+          carNumber,
+          custAddress,
+          serviceName,
+          servicePrice,
+        },
+        {
+          headers: authHeader(),
+        }
+      )
+      .then((response) => {
+        return response.data.message;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  findMyOrders(id) {
+    return axios
+      .get(CUST_ORDER + `findOrders/${id}`, {
+        headers: authHeader(),
+      })
+      .then((response) => {
+        return response.data.orders;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  findCustomerById(id) {
+    return axios
+      .get(`http://localhost:8080/customer/account/findCustById/${id}`)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+export default new CustomerService();
